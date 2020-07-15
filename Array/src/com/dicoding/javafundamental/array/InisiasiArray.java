6147f4ac479427bc6919a39cded668691315e0e5
Farlan <faridmaulana1921@gmail.com>
2020-07-15 21:47:02 +0700

    Materi Array(inisiasi array, looping array, multi dimensional array, index out of bounds)

---
+++ Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,42 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray {
+    public static void main(String[] args) {
+
+        // cara pertama inisiasi array
+        int[] arrInt1 = new int[]{1, 2, 3, 4, 5, 6};
+
+        System.out.println(arrInt1[0]);
+        System.out.println(arrInt1[1]);
+        System.out.println(arrInt1[2]);
+        System.out.println(arrInt1[3]);
+        System.out.println(arrInt1[4]);
+        System.out.println(arrInt1[5]);
+
+        // cara kedua inisiasi array : per elemen
+        int[] arrInt2 = new int[6];
+        arrInt2[0] = 1;
+        arrInt2[1] = 2;
+        arrInt2[2] = 3;
+        arrInt2[3] = 4;
+        arrInt2[4] = 5;
+        arrInt2[5] = 6;
+
+        System.out.println(arrInt2[0]);
+        System.out.println(arrInt2[1]);
+        System.out.println(arrInt2[2]);
+        System.out.println(arrInt2[3]);
+        System.out.println(arrInt2[4]);
+        System.out.println(arrInt2[5]);
+
+        // cara ketiga inisiasi array : menyederhanakan dari cara pertama
+        int[] arrInt = {1, 2, 3, 4, 5, 6};
+
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+        System.out.println(arrInt[5]);
+    }
+}
