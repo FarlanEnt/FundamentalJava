59194058929e5ec4698954ba4e102e58a6ed8551
Farlan <faridmaulana1921@gmail.com>
2020-07-18 15:03:39 +0700

    Materi Class(Class & Construktor)

---
+++ Class/src/com/dicoding/javafundamental/kelas/KucingAnggora.java
@@ -0,0 +1,19 @@
+package com.dicoding.javafundamental.kelas;
+
+public class KucingAnggora {
+    // class body
+
+    // contoh no-argument construktor
+    /* public KucingAnggora() {
+
+    } */
+
+    // contoh parameteriez construktor
+    String nama_kucing;
+    int berat_badan;
+
+    public KucingAnggora(String nama, int berat) {
+        nama_kucing = nama;
+        berat_badan = berat;
+    }
+}
